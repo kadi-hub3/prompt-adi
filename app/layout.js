18cce1efdfa87fbd9efc34707103bb2997e89c71
Kadi M <kicktheball1@gmail.com>
2024-06-17 18:22:42 +0200

    Authentication & DB

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,7 @@ const RootLayout = ({children}) => {
   return (
 <html>
   <body>
+    <Provider>
     <div className="main">
       <div className="gradient"></div>
     </div>
@@ -17,6 +18,7 @@ const RootLayout = ({children}) => {
       <Nav />
       {children}
     </main>
+    </Provider>
   </body>
 </html>
   )
